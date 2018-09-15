60b3b001be44882f0e5e491fbac89739bec598c7
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-09-15 00:37:22 -0400

    Update initdb-postgis.sh

--- postgis/initdb-postgis.sh
+++ postgis/initdb-postgis.sh
@@ -25,5 +25,7 @@ done
 
 #import Melbourne city
 osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /Melbourne.osm.pbf
+osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /australia-oceania-latest.osm.pbf
+osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /australia-oceania/australia-latest.osm.pbf
 
 touch /var/lib/postgresql/data/DB_INITED
