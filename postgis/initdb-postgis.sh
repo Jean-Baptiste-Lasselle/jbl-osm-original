ecd0e85323ae89f8ebc00b90db0f00eb2583f4a6
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-22 18:31:33 +0200

    Update initdb-postgis.sh
    En suivant les indications de : 
      https://github.com/openstreetmap/osm2pgsql/issues/596#issuecomment-227250358
    Je re-configure les paramètres de connexionà  PostGIS de `osm2pgsql` tel qu'indiqué par la [documentation de libpg](https://www.postgresql.org/docs/current/static/libpq-envars.html)

--- postgis/initdb-postgis.sh
+++ postgis/initdb-postgis.sh
@@ -35,9 +35,28 @@ for DB in template_postgis "$POSTGRES_DB"; do
 EOSQL
 done
 
+
+export PGHOST=localhost
+export PGPASSWORD=$POSTGRES_PASSWORD
+export PGUSER=$POSTGRES_USER
+
 #import Melbourne city
 # osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /Melbourne.osm.pbf
 osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /australia-oceania-latest.osm.pbf
 # osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /australia-oceania/australia-latest.osm.pbf
 
 touch /var/lib/postgresql/data/DB_INITED
+
+
+# The following environment variables can be used to select default connection parameter values, which will be used by PQconnectdb, PQsetdbLogin and PQsetdb if no value is directly specified by the calling code. These are useful to avoid hard-coding database connection information into simple client applications, for example.
+
+#     PGHOST behaves the same as the host connection parameter.
+#     PGHOSTADDR behaves the same as the hostaddr connection parameter. This can be set instead of or in addition to PGHOST to avoid DNS lookup overhead.
+#     PGPORT behaves the same as the port connection parameter.
+#     PGDATABASE behaves the same as the dbname connection parameter.
+#     PGUSER behaves the same as the user connection parameter.
+#     PGPASSWORD behaves the same as the password connection parameter. Use of this environment variable is not recommended for security reasons, as some operating systems allow non-root users to see process environment variables via ps; instead consider using a password file (see Section 34.15).
+#     PGPASSFILE behaves the same as the passfile connection parameter.
+#     PGSERVICE behaves the same as the service connection parameter.
+#     PGSERVICEFILE specifies the name of the per-user connection service file. If not set, it defaults to ~/.pg_service.conf (see Section 34.16).
+#     PGOPTIONS behaves the same as the options connection parameter.
