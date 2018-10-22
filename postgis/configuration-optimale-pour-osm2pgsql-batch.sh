5dd06a220e1045c938ed2cd2b313815f6f2da1c7
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-22 11:42:05 +0200

    Update configuration-optimale-pour-osm2pgsql-batch.sh

--- postgis/configuration-optimale-pour-osm2pgsql-batch.sh
+++ postgis/configuration-optimale-pour-osm2pgsql-batch.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-# Attention mon poulet, dans le Dockerfile, il faut être rcine des Ents, pour faire ce genre de trucs, okay?
+# Attention mon poulet, dans le Dockerfile, il faut être arcine des Ents, pour faire ce genre de trucs, okay?
+echo "Attention mon poulet, dans le Dockerfile, il faut être arcine des Ents, pour faire ce genre de trucs, okay?"
 echo "# Overcommit settings to allow faster osm2pgsql imports"
 tee /etc/sysctl.d/60-overcommit.conf <<EOF
 vm.overcommit_memory=1
