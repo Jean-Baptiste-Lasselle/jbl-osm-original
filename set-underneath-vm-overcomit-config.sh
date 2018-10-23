c988367dad883f03746dadc2e379f81a7d9e6c1e
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-23 17:57:07 +0200

    Update set-underneath-vm-overcomit-config.sh

--- set-underneath-vm-overcomit-config.sh
+++ set-underneath-vm-overcomit-config.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # set-underneath-vm-overcomit-config.sh
-# Attention mon poulet, dans le Dockerfile, il faut être arcine des Ents, pour faire ce genre de trucs, okay?
-echo "Attention mon poulet, dans le Dockerfile, il faut être racine des Ents, pour faire ce genre de trucs, okay?"
+# Attention mon poulet, il faut être arcine des Ents, pour faire ce genre de trucs, okay?
+echo "Attention mon poulet, il faut être racine des Ents, pour faire ce genre de trucs, okay?"
 echo "# Overcommit settings to allow faster osm2pgsql imports"
 sudo tee /etc/sysctl.d/60-overcommit.conf <<EOF
 vm.overcommit_memory=1
