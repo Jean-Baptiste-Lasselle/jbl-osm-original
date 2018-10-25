c78e8eca900d5ba2131629a89d701a29440c8525
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-25 15:59:59 +0200

    Update compile_style.sh

--- renderer/scripts/compile_style.sh
+++ renderer/scripts/compile_style.sh
@@ -5,10 +5,20 @@
 
 carto /openstreetmap-carto/project.mml > /map_data/stylesheet_.xml
 
-DS='<Parameter name=\"dbname\"><![CDATA[gis]]><\/Parameter>\
-    <Parameter name=\"host\"><![CDATA[postgis]]><\/Parameter>\
-    <Parameter name=\"port\"><![CDATA[5432]]><\/Parameter>\
-    <Parameter name=\"user\"><![CDATA[postgres]]><\/Parameter>\
-    <Parameter name=\"password\"><![CDATA[postgres]]><\/Parameter>'
+echo "  "
+echo " ----- "
+echo " VERIF JBL dans complie_style.sh de monsieur catactrophe aka 'domman84' [MAPNIK_POSTGRES_DB_HOST=$MAPNIK_POSTGRES_DB_HOST]"
+echo " VERIF JBL dans complie_style.sh de monsieur catactrophe aka 'domman84' [MAPNIK_POSTGRES_DB_PORT_NO=$MAPNIK_POSTGRES_DB_PORT_NO]"
+echo " VERIF JBL dans complie_style.sh de monsieur catactrophe aka 'domman84' [MAPNIK_POSTGRES_DB=$MAPNIK_POSTGRES_DB]"
+echo " VERIF JBL dans complie_style.sh de monsieur catactrophe aka 'domman84' [MAPNIK_POSTGRES_USER=$MAPNIK_POSTGRES_USER]"
+echo " VERIF JBL dans complie_style.sh de monsieur catactrophe aka 'domman84' [MAPNIK_POSTGRES_PASSWORD=$MAPNIK_POSTGRES_PASSWORD]"
+echo " ----- "
+echo "  "
+
+DS='<Parameter name=\"$MAPNIK_POSTGRES_DB\"><![CDATA[gis]]><\/Parameter>\
+    <Parameter name=\"MAPNIK_POSTGRES_DB_HOST\"><![CDATA[postgis]]><\/Parameter>\
+    <Parameter name=\"$MAPNIK_POSTGRES_DB_PORT_NO\"><![CDATA[5432]]><\/Parameter>\
+    <Parameter name=\"$MAPNIK_POSTGRES_USER\"><![CDATA[postgres]]><\/Parameter>\
+    <Parameter name=\"$MAPNIK_POSTGRES_PASSWORD\"><![CDATA[postgres]]><\/Parameter>'
 sed "s/<Parameter name=\"dbname\">.*<\/Parameter>/${DS}/" /map_data/stylesheet_.xml > /map_data/stylesheet.xml
 rm /map_data/stylesheet_.xml
