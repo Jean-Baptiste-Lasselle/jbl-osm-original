a6994f39107a03dc9b5e1f942380941c2fa167b3
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-25 14:55:46 +0200

    Update initdb-postgis.sh

--- postgis/initdb-postgis.sh
+++ postgis/initdb-postgis.sh
@@ -59,7 +59,19 @@ export PGUSER=$POSTGRES_USER
 # osm2pgsql --style /openstreetmap-carto/openstreetmap-carto.style -d gis -U postgres -k --slim /australia-oceania/australia-latest.osm.pbf
 
 # --- skipping all his shit 
-osm2pgsql -U $POSTGRES_USER  --create --database gis /australia-oceania-latest.osm.pbf 
+# --- En vertu de [https://github.com/openstreetmap/osm2pgsql/blob/master/docs/usage.md#database-options]  : 
+# " This should only be used on full planet imports or very large extracts (e.g. Europe) but in 
+#  those situations offers significant space savings and speed increases, particularly on mechanical
+#   drives. The file takes approximately 8 bytes * maximum node ID, or about 23 GiB, regardless of the size of the extract. "
+# --- > je vais utiliser l'options "--flat-nodes", parce que j'importe la terre entière, ici un fihcier de 42 Go quand même...
+# --- En vertu de [https://github.com/openstreetmap/osm2pgsql/blob/master/docs/usage.md#middle-layer-options] : 
+# " A --slim --drop import is generally the fastest way to import the planet if updates are not required. "
+# Je vais utiliser aussi la combinaison d'options [ --slim --drop]
+# --- En vertu de [https://github.com/openstreetmap/osm2pgsql/blob/master/docs/usage.md#output-columns-options] : 
+# " --style specifies the location of the style file. This defines what columns are created, what tags denote #
+#  areas, and what tags can be ignored. The default.style contains more documentation on this file. "
+# --- > je vais utiliser l'options " --style /openstreetmap-carto/openstreetmap-carto.style " pour appliquer le style d'openstreemp carto
+osm2pgsql -U $POSTGRES_USER --create --flat-nodes --slim --drop --style /openstreetmap-carto/openstreetmap-carto.style --database gis /australia-oceania-latest.osm.pbf 
 
 
 # The following environment variables can be used to select default connection parameter values, which will be used by PQconnectdb, PQsetdbLogin and PQsetdb if no value is directly specified by the calling code. These are useful to avoid hard-coding database connection information into simple client applications, for example.
