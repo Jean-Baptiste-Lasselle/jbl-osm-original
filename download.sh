8a32bda9014fa8899f5a306415262af94fce0742
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-25 20:37:14 +0200

    Update download.sh
    modification pour modification/accélération du cycle devops

--- download.sh
+++ download.sh
@@ -36,70 +36,157 @@ mkdir -p ${SHAPES_DIR}/simplified-water-polygons-complete-3857
 mkdir -p ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land
 mkdir -p ${SHAPES_DIR}/water-polygons-split-3857
 
-# world_boundaries
-echo "downloading world_boundaries..."
-curl -z "${SHAPES_DIR}/world_boundaries-spherical.tgz" -L -o "${SHAPES_DIR}/world_boundaries-spherical.tgz" "https://planet.openstreetmap.org/historical-shapefiles/world_boundaries-spherical.tgz"
-echo "expanding world_boundaries..."
-tar -xzf ${SHAPES_DIR}/world_boundaries-spherical.tgz -C ${SHAPES_DIR}/
-
-# simplified-water-polygons-complete-3857
-echo "downloading simplified-water-polygons-complete-3857..."
-curl -z "${SHAPES_DIR}/simplified-water-polygons-complete-3857.zip" -L -o "${SHAPES_DIR}/simplified-water-polygons-complete-3857.zip" "http://data.openstreetmapdata.com/simplified-water-polygons-complete-3857.zip"
-echo "simplified-water-polygons-complete-3857..."
-unzip $UNZIP_OPTS ${SHAPES_DIR}/simplified-water-polygons-complete-3857.zip \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.shp \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.shx \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.prj \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.dbf \
-  simplified-water-polygons-complete-3857/simplified_water_polygons.cpg \
-  -d ${SHAPES_DIR}/
-
-# ne_110m_admin_0_boundary_lines_land
-echo "downloading ne_110m_admin_0_boundary_lines_land..."
-curl -z ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land.zip -L -o ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_boundary_lines_land.zip
-echo "expanding ne_110m_admin_0_boundary_lines_land..."
-unzip $UNZIP_OPTS ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land.zip \
-  ne_110m_admin_0_boundary_lines_land.shp \
-  ne_110m_admin_0_boundary_lines_land.shx \
-  ne_110m_admin_0_boundary_lines_land.prj \
-  ne_110m_admin_0_boundary_lines_land.dbf \
-  -d ${SHAPES_DIR}/ne_110m_admin_0_boundary_lines_land/
-
-# water-polygons-split-3857
-echo "downloading water-polygons-split-3857..."
-curl -z "${SHAPES_DIR}/water-polygons-split-3857.zip" -L -o "${SHAPES_DIR}/water-polygons-split-3857.zip" "http://data.openstreetmapdata.com/water-polygons-split-3857.zip"
-echo "expanding water-polygons-split-3857..."
-unzip $UNZIP_OPTS ${SHAPES_DIR}/water-polygons-split-3857.zip \
-  water-polygons-split-3857/water_polygons.shp \
-  water-polygons-split-3857/water_polygons.shx \
-  water-polygons-split-3857/water_polygons.prj \
-  water-polygons-split-3857/water_polygons.dbf \
-  water-polygons-split-3857/water_polygons.cpg \
-  -d ${SHAPES_DIR}/
-
-# antarctica-icesheet-polygons-3857
-echo "downloading antarctica-icesheet-polygons-3857..."
-curl -z "${SHAPES_DIR}/antarctica-icesheet-polygons-3857.zip" -L -o "${SHAPES_DIR}/antarctica-icesheet-polygons-3857.zip" "http://data.openstreetmapdata.com/antarctica-icesheet-polygons-3857.zip"
-echo "expanding antarctica-icesheet-polygons-3857..."
-unzip $UNZIP_OPTS ${SHAPES_DIR}/antarctica-icesheet-polygons-3857.zip \
-  antarctica-icesheet-polygons-3857/icesheet_polygons.shp \
-  antarctica-icesheet-polygons-3857/icesheet_polygons.shx \
-  antarctica-icesheet-polygons-3857/icesheet_polygons.prj \
-  antarctica-icesheet-polygons-3857/icesheet_polygons.dbf \
-  -d ${SHAPES_DIR}/
-
-# antarctica-icesheet-outlines-3857
-echo "downloading antarctica-icesheet-outlines-3857..."
-curl -z "${SHAPES_DIR}/antarctica-icesheet-outlines-3857.zip" -L -o "${SHAPES_DIR}/antarctica-icesheet-outlines-3857.zip" "http://data.openstreetmapdata.com/antarctica-icesheet-outlines-3857.zip"
-echo "expanding antarctica-icesheet-outlines-3857..."
-unzip $UNZIP_OPTS ${SHAPES_DIR}/antarctica-icesheet-outlines-3857.zip \
-  antarctica-icesheet-outlines-3857/icesheet_outlines.shp \
-  antarctica-icesheet-outlines-3857/icesheet_outlines.shx \
-  antarctica-icesheet-outlines-3857/icesheet_outlines.prj \
-  antarctica-icesheet-outlines-3857/icesheet_outlines.dbf \
-  -d ${SHAPES_DIR}/
-
-rm renderer/shapes/*.zip
-rm renderer/shapes/*.tgz
 
 
+cd $PBF_VAULT_HOME
+
+if [ -f $PBF_VAULT_HOME/world_boundaries-spherical.tgz ]; then
+   echo "Le fichier [$PBF_VAULT_HOME/world_boundaries-spherical.tgz] existe déjà : ";
+   ls -all $PBF_VAULT_HOME/world_boundaries-spherical.tgz
+else 
+   echo "Téléchargement du fichier [$PBF_VAULT_HOME/world_boundaries-spherical.tgz] ... "
+   # world_boundaries
+   echo "downloading world_boundaries  [${SHAPES_DIR}/world_boundaries-spherical.tgz] into [PBF_VAULT_HOME=$PBF_VAULT_HOME] ..."
+   curl -z "${SHAPES_DIR}/world_boundaries-spherical.tgz" -L -o "${PBF_VAULT_HOME}/world_boundaries-spherical.tgz" "https://planet.openstreetmap.org/historical-shapefiles/world_boundaries-spherical.tgz"
+   echo "expanding world_boundaries into [PBF_VAULT_HOME=$PBF_VAULT_HOME] ..."
+   tar -xzf ${SHAPES_DIR}/world_boundaries-spherical.tgz -C ${PBF_VAULT_HOME}/
+   echo " Téléchargement du fichier [$PBF_VAULT_HOME/world_boundaries-spherical.tgz] terminé : "
+   ls -all $PBF_VAULT_HOME/world_boundaries-spherical.tgz
+fi
+
+
+
+# cp f $PBF_VAULT_HOME/world_boundaries-spherical.tgz ${SHAPES_DIR}/world_boundaries-spherical.tgz 
+# cf. fin du script (end of script) : cp -Rf $PBF_VAULT_HOME/ ${SHAPES_DIR}/ 
+
+
+# Je ne vérifie pas l'existence des fichiers *.shp *.shx *.prj *.dnf *.cpg , je me contente de l'existence du zip
+# (ça se dézippe, des shapefiles...?)
+# I don't check existence of  *.shp *.shx *.prj *.dnf *.cpg files, just rely on zip's
+if [ -f $PBF_VAULT_HOME/simplified-water-polygons-complete-3857.zip ]; then
+   echo "Le fichier [$PBF_VAULT_HOME/simplified-water-polygons-complete-3857.zip] existe déjà : ";
+   ls -all $PBF_VAULT_HOME/simplified-water-polygons-complete-3857.zip
+else 
+   echo "Téléchargement du fichier [$PBF_VAULT_HOME/simplified-water-polygons-complete-3857.zip] ... "
+   # simplified-water-polygons-complete-3857
+   echo "downloading simplified-water-polygons-complete-3857..."
+   curl -z "${PBF_VAULT_HOME}/simplified-water-polygons-complete-3857.zip" -L -o "${PBF_VAULT_HOME}/simplified-water-polygons-complete-3857.zip" "http://data.openstreetmapdata.com/simplified-water-polygons-complete-3857.zip"
+   echo "simplified-water-polygons-complete-3857..."
+   unzip $UNZIP_OPTS ${PBF_VAULT_HOME}/simplified-water-polygons-complete-3857.zip \
+     simplified-water-polygons-complete-3857/simplified_water_polygons.shp \
+     simplified-water-polygons-complete-3857/simplified_water_polygons.shx \
+     simplified-water-polygons-complete-3857/simplified_water_polygons.prj \
+     simplified-water-polygons-complete-3857/simplified_water_polygons.dbf \
+     simplified-water-polygons-complete-3857/simplified_water_polygons.cpg \
+     -d ${PBF_VAULT_HOME}/
+   echo " Téléchargement du fichier [${PBF_VAULT_HOME}/simplified-water-polygons-complete-3857.zip] terminé : "
+   ls -all ${PBF_VAULT_HOME}/simplified-water-polygons-complete-3857.zip
+fi
+
+
+
+# Je ne vérifie pas l'existence des fichiers *.shp *.shx *.prj *.dnf *.cpg , je me contente de l'existence du zip
+# (ça se dézippe, des shapefiles...?)
+# I don't check existence of  *.shp *.shx *.prj *.dnf *.cpg files, just rely on zip's
+if [ -f ${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip ]; then
+   echo "Le fichier [${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip] existe déjà : ";
+   ls -all ${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip
+else 
+   echo "Téléchargement du fichier [${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip] ... "
+   # ne_110m_admin_0_boundary_lines_land
+   echo "downloading ne_110m_admin_0_boundary_lines_land..."
+   curl -z ${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip -L -o ${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip https://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/cultural/ne_110m_admin_0_boundary_lines_land.zip
+   echo "expanding ne_110m_admin_0_boundary_lines_land..."
+   unzip $UNZIP_OPTS ${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip \
+     ne_110m_admin_0_boundary_lines_land.shp \
+     ne_110m_admin_0_boundary_lines_land.shx \
+     ne_110m_admin_0_boundary_lines_land.prj \
+     ne_110m_admin_0_boundary_lines_land.dbf \
+     -d ${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land/
+   echo " Téléchargement du fichier [${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip] terminé : "
+   ls -all ${PBF_VAULT_HOME}/ne_110m_admin_0_boundary_lines_land.zip
+fi
+
+
+
+
+# Je ne vérifie pas l'existence des fichiers *.shp *.shx *.prj *.dnf *.cpg , je me contente de l'existence du zip
+# (ça se dézippe, des shapefiles...?)
+# I don't check existence of  *.shp *.shx *.prj *.dnf *.cpg files, just rely on zip's
+if [ -f ${PBF_VAULT_HOME}/water-polygons-split-3857.zip ]; then
+   echo "Le fichier [${PBF_VAULT_HOME}/water-polygons-split-3857.zip] existe déjà : ";
+   ls -all ${PBF_VAULT_HOME}/water-polygons-split-3857.zip
+else 
+   echo "Téléchargement du fichier [${PBF_VAULT_HOME}/water-polygons-split-3857.zip] ... "
+   # water-polygons-split-3857
+   echo "downloading water-polygons-split-3857..."
+   curl -z "${PBF_VAULT_HOME}/water-polygons-split-3857.zip" -L -o "${PBF_VAULT_HOME}/water-polygons-split-3857.zip" "http://data.openstreetmapdata.com/water-polygons-split-3857.zip"
+   echo "expanding water-polygons-split-3857..."
+   unzip $UNZIP_OPTS ${PBF_VAULT_HOME}/water-polygons-split-3857.zip \
+     water-polygons-split-3857/water_polygons.shp \
+     water-polygons-split-3857/water_polygons.shx \
+     water-polygons-split-3857/water_polygons.prj \
+     water-polygons-split-3857/water_polygons.dbf \
+     water-polygons-split-3857/water_polygons.cpg \
+  -d ${PBF_VAULT_HOME}/
+   echo " Téléchargement du fichier [${PBF_VAULT_HOME}/water-polygons-split-3857.zip] terminé : "
+   ls -all ${PBF_VAULT_HOME}/water-polygons-split-3857.zip
+fi
+
+
+
+# Je ne vérifie pas l'existence des fichiers *.shp *.shx *.prj *.dnf *.cpg , je me contente de l'existence du zip
+# (ça se dézippe, des shapefiles...?)
+# I don't check existence of  *.shp *.shx *.prj *.dnf *.cpg files, just rely on zip's
+if [ -f ${PBF_VAULT_HOME}/icesheet_polygons.zip ]; then
+   echo "Le fichier [${PBF_VAULT_HOME}/icesheet_polygons.zip] existe déjà : ";
+   ls -all ${PBF_VAULT_HOME}/icesheet_polygons.zip
+else 
+   echo "Téléchargement du fichier [${PBF_VAULT_HOME}/icesheet_polygons.zip] ... "
+   # antarctica-icesheet-polygons-3857
+   echo "downloading antarctica-icesheet-polygons-3857..."
+   curl -z "${PBF_VAULT_HOME}/antarctica-icesheet-polygons-3857.zip" -L -o "${PBF_VAULT_HOME}/antarctica-icesheet-polygons-3857.zip" "http://data.openstreetmapdata.com/antarctica-icesheet-polygons-3857.zip"
+   echo "expanding antarctica-icesheet-polygons-3857..."
+   unzip $UNZIP_OPTS ${PBF_VAULT_HOME}/antarctica-icesheet-polygons-3857.zip \
+     antarctica-icesheet-polygons-3857/icesheet_polygons.shp \
+     antarctica-icesheet-polygons-3857/icesheet_polygons.shx \
+     antarctica-icesheet-polygons-3857/icesheet_polygons.prj \
+     antarctica-icesheet-polygons-3857/icesheet_polygons.dbf \
+     -d ${PBF_VAULT_HOME}/
+   echo " Téléchargement du fichier [${PBF_VAULT_HOME}/icesheet_polygons.zip] terminé : "
+   ls -all ${PBF_VAULT_HOME}/icesheet_polygons.zip
+fi
+
+
+
+# Je ne vérifie pas l'existence des fichiers *.shp *.shx *.prj *.dnf *.cpg , je me contente de l'existence du zip
+# (ça se dézippe, des shapefiles...?)
+# I don't check existence of  *.shp *.shx *.prj *.dnf *.cpg files, just rely on zip's
+if [ -f ${PBF_VAULT_HOME}/icesheet_outlines.zip ]; then
+   echo "Le fichier [${PBF_VAULT_HOME}/icesheet_outlines.zip] existe déjà : ";
+   ls -all ${PBF_VAULT_HOME}/icesheet_outlines.zip
+else 
+   echo "Téléchargement du fichier [${PBF_VAULT_HOME}/icesheet_outlines.zip] ... "
+   # antarctica-icesheet-outlines-3857
+   echo "downloading antarctica-icesheet-outlines-3857..."
+   curl -z "${PBF_VAULT_HOME}/antarctica-icesheet-outlines-3857.zip" -L -o "${PBF_VAULT_HOME}/antarctica-icesheet-outlines-3857.zip" "http://data.openstreetmapdata.com/antarctica-icesheet-outlines-3857.zip"
+   echo "expanding antarctica-icesheet-outlines-3857..."
+   unzip $UNZIP_OPTS ${PBF_VAULT_HOME}/antarctica-icesheet-outlines-3857.zip \
+     antarctica-icesheet-outlines-3857/icesheet_outlines.shp \
+     antarctica-icesheet-outlines-3857/icesheet_outlines.shx \
+     antarctica-icesheet-outlines-3857/icesheet_outlines.prj \
+     antarctica-icesheet-outlines-3857/icesheet_outlines.dbf \
+     -d ${PBF_VAULT_HOME}/
+   echo " Téléchargement du fichier [${PBF_VAULT_HOME}/icesheet_outlines.zip] terminé : "
+   ls -all ${PBF_VAULT_HOME}/icesheet_outlines.zip
+fi
+
+
+
+# rm renderer/shapes/*.zip
+# rm renderer/shapes/*.tgz
+
+
+cd $WHEREEVERIWASBEFORE
+cp -f $PBF_VAULT_HOME/*.* ${SHAPES_DIR}
+cp -Rf $PBF_VAULT_HOME/* ${SHAPES_DIR}
