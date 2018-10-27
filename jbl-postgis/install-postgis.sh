bf2b8f3efb987712ecf62b3dfa9a0d3dcb8afa7a
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-27 02:01:51 -0400

    Update install-postgis.sh

--- jbl-postgis/install-postgis.sh
+++ jbl-postgis/install-postgis.sh
@@ -13,13 +13,14 @@ apt-get update -y
 # sudo apt install postgresql-10-postgis-2.4 
 # sudo apt install postgresql-10-postgis-scripts
 
-
-apt-get update \
-    && apt-get install -y --no-install-recommends \
-         postgresql-$PG_MAJOR-postgis-$POSTGIS_MAJOR=$POSTGIS_VERSION \
-         postgresql-$PG_MAJOR-postgis-$POSTGIS_MAJOR-scripts=$POSTGIS_VERSION \
-         postgis=$POSTGIS_VERSION \
-         && rm -rf /var/lib/apt/lists/*
+apt-get installl -y postgis
+rm -rf /var/lib/apt/lists/*
+# apt-get update -y \
+#     && apt-get install -y --no-install-recommends \
+#          postgresql-$PG_MAJOR-postgis-$POSTGIS_MAJOR=$POSTGIS_VERSION \
+#          postgresql-$PG_MAJOR-postgis-$POSTGIS_MAJOR-scripts=$POSTGIS_VERSION \
+#          postgis=$POSTGIS_VERSION \
+#          && rm -rf /var/lib/apt/lists/*
          
 # To get the commandline tools shp2pgsql, raster2pgsql you need to do this ? Well I'll let taht aside, I have tests to run
 # sudo apt-get install postgis
