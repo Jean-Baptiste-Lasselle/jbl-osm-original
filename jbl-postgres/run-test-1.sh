cf73fc642e478f63b9b4e948fb846d24bc157679
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-28 19:37:20 -0400

    Update run-test-1.sh

--- jbl-postgres/run-test-1.sh
+++ jbl-postgres/run-test-1.sh
@@ -1,1 +1,38 @@
-c
+#!/bin/bash
+
+# -> Un processus /bin/bash est donc l'environnement d'exécution du test
+
+# What I test here :
+# ...
+# 
+# 
+
+
+# - TEST ENV. CLEAN UP
+docker-compose down --rmi all
+
+# - TEST SETUP
+# I have setup my stup resources (of test `N` in `./tests-resources/N/`, where N=1, 2, 3 ...
+
+cp ./.env ./.env.clone
+rm -f ./.env
+cp ./tests-resources/1/.env ./.env
+docker-compose up -d
+
+# - TEST RUN
+# What I expect as the result test :
+# ...
+# 
+# 
+
+# export NOM_CONTENEUR_TESTRUNNER=test1-jbl-postgres-docker
+export NOM_CONTENEUR_TESTRUNNER=$(cat ./docker-compose.yml|grep container|awk -F ':' '{print $2}')
+echo "  "
+echo " VERIF ENV. TEST RUN : [NOM_CONTENEUR_TESTRUNNER=$NOM_CONTENEUR_TESTRUNNER] "
+echo "  "
+export JEU_OPTIONS=""
+export JEU_OPTIONS="$JEU_OPTIONS -e POSTGRES_USER=$POSTGRES_USER"
+export JEU_OPTIONS="$JEU_OPTIONS -e POSTGRES_PASSSWORD=$POSTGRES_PASSSWORD"
+
+# - TEST TEAR DOWN
+docker-compose down --rmi all
