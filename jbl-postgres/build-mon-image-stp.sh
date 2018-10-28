0fcc8bc5109f0114da3665a5cc77ec4e7ddd9d5b
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-27 20:01:55 -0400

    Update build-mon-image-stp.sh

--- jbl-postgres/build-mon-image-stp.sh
+++ jbl-postgres/build-mon-image-stp.sh
@@ -27,20 +27,33 @@ sudo docker build -t $NOM_DE_MON_IMAGE .
 # -       - #
 # -  RUN  - #
 # -       - #
-export RESEAU_DOCKER_DEVOPS_TESTS=reseau-tests-devops
-# - Je créée un réseau docker de type bridge
-docker network create --driver bridge $RESEAU_DOCKER_DEVOPS_TESTS
-# - Je lance 2 conteneur dans ce réseau, différant seulement par les variables d'environnements / build args 
-export RESEAU_DOCKER_DEVOPS_TESTS=reseau-tests-devops2
+export RESEAU_DOCKER_DEVOPS_TESTS=""
 export JEU_OPTIONS=""
+
+
+
+# 
+# [TESTS] pour chaque test :
+# - 
+# - Je lance 1 conteneur dans le réseau, différant seulement par les variables d'environnements / build args 
+
+# - Environnement du test
+export RESEAU_DOCKER_DEVOPS_TESTS=reseau-tests-devops1
 export JEU_OPTIONS="$JEU_OPTIONS -e POSTGRES_USER=kytes"
 export JEU_OPTIONS="$JEU_OPTIONS -e POSTGRES_PASSSWORD=kytes"
-# export JEU_OPTIONS="$JEU_OPTIONS -e VVV=VVV"
-# export JEU_OPTIONS="$JEU_OPTIONS -e VVV=VVV"
-# export JEU_OPTIONS="$JEU_OPTIONS -e VVV=VVV"
 
-docker run --name test-jbl-postgres-docker2 --network $RESEAU_DOCKER_DEVOPS_TESTS $JEU_OPTIONS -d $NOM_DE_MON_IMAGE
-
+# - SETUP 
+# - Je créée un réseau docker de type bridge
+docker network create --driver bridge $RESEAU_DOCKER_DEVOPS_TESTS
+# - J'exéute le test
+docker run --name test1-jbl-postgres-docker --network $RESEAU_DOCKER_DEVOPS_TESTS $JEU_OPTIONS -d $NOM_DE_MON_IMAGE
+# - TEAR DOWN 
+# docker stop test1-jbl-postgres-docker && docker rm test1-jbl-postgres-docker
 
 # Test 1 : me connecter à la BDD avecle client postgresql
 docker run -it --rm --network postgres-network postgresondocker:9.3 psql -h postgresondocker -U postgresondocker --password
+
+
+# !! -> !! FINAL TEARDOWN
+# docker rmi $NOM_DE_MON_IMAGE
+# sudo docker system prune -f
