f4519bf2ec0a5418095ceb8c79bbdbefea5b224a
Jean-Baptiste-Lasselle <35227860+Jean-Baptiste-Lasselle@users.noreply.github.com>
2018-10-28 20:37:41 -0400

    Update run-test-1.sh

--- jbl-postgres/run-test-1.sh
+++ jbl-postgres/run-test-1.sh
@@ -31,25 +31,21 @@ export NOM_CONTENEUR_TESTRUNNER=$(cat ./.env |grep NOM_CONTENEUR_EXECUTION_TESTS
 echo "  "
 echo " VERIF ENV. TEST RUN : [NOM_CONTENEUR_TESTRUNNER=$NOM_CONTENEUR_TESTRUNNER] "
 echo "  "
-export JEU_OPTIONS=""
-export JEU_OPTIONS="$JEU_OPTIONS -e POSTGRES_USER=$POSTGRES_USER"
-export JEU_OPTIONS="$JEU_OPTIONS -e POSTGRES_PASSSWORD=$POSTGRES_PASSSWORD"
-echo "   "
-echo " - ==>>> AVANT DOCKER RUN (setup test)"
-echo "   "
-echo " - "
-echo " VERIF : [JEU_OPTIONS=$JEU_OPTIONS]"
-echo " - "
-echo " VERIF : [NOM_DE_MON_IMAGE=$NOM_DE_MON_IMAGE]"
-echo " - "
-echo " You currently have those test images : "
-echo "   "
-docker images|grep postgres
-echo "   "
+docker ps -a |grep $NOM_CONTENEUR_TESTRUNNER
+echo "  "
 read ATTENDS1
 
-docker run -itd --name $NOM_CONTENEUR_TESTRUNNER --restart=always --network $RESEAU_DOCKER_DEVOPS_TESTS $JEU_OPTIONS $NOM_DE_MON_IMAGE
-# 
+clear
+
+echo "  Utilisateur par défaut dans le conteneur : "
+docker exec -it $NOM_CONTENEUR_TESTRUNNER whoami
+echo "  Authentification auserveur PostGreSQL, en utilsiant le client psql, et avec l'utilisateur admin initial configuré dans le 'docker-compose.yml'  : "
+docker exec -it $NOM_CONTENEUR_TESTRUNNER sh -c "psql -h $NOM_CONTENEUR_TESTRUNNER -U postgres --password"
+
+echo " Vérifions que l'on peut utiliser la varible d'environnement 'PGPASSWORD' pour fixer le contexte d'authentification psql () "
+echo " (pour s'authentifier sans saisie interactive de mot de passe) : "
+docker exec -it $NOM_CONTENEUR_TESTRUNNER sh -c "export PGPASSWORD=$POSTGRES_PASSWORD && psql -h $NOM_CONTENEUR_TESTRUNNER -U postgres "
+
 # But (TEST RESULT OK) du test : arriver à me connecter à la BDD avec le client postgresql
 # 
 # d'autres tests : se connecter depuis un autre hôte réseau, un autre conteneur dans le réseau bridge Docker.
@@ -58,16 +54,24 @@ echo " Your tests logs : "
 docker logs $NOM_CONTENEUR_TESTRUNNER
 
 echo " - "
-echo " To TEAR DOWN : "
+echo " Next test : Press Enter key "
 echo " - "
-docker images|grep postgres
+echo "           "
 echo " - "
 read ATTENDS1
+docker-compose down --rmi all
 # 
 # - TEST TEAR DOWN
 # docker-compose down --rmi all > ./teardown.log
 # cat ./teardown.log 
 # 
 
+echo " - "
+echo " You reached last test : Press Enter key to exit test suite "
+echo " - "
+echo "           "
+echo " - "
+read ATTENDS1
+docker-compose down --rmi all
 rm -f ./.env
 cp ./.env.clone ./.env
